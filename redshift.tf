ce3a6f0c482add490ef45ae2a359c449f4730a22
Ma Chi <hellomarch@gmail.com>
2019-09-18 14:27:14 +0000

    1. comment in English 2. Add a FinalClusterSnapshotIdentifier

--- redshift.tf
+++ redshift.tf
@@ -1,37 +1,50 @@
-# 使用aws的Virginia区域
+#Use US West 2  
 provider "aws" {
   region     = "us-west-2"
-  #用户token在配置文件里,不在此处声明
+  #AWS token is stored in ~/.aws/, not declared here 
 }
 
-#安全组的规则声明如下
+#pre-provisioned vpc
+variable "vpc_id" {
+  default = "vpc-c5bd6fbd"
+}
 
+# port number of database 
 variable "port_number"{
   default = 5439
   description = "port no. for psql"
 }
 
+#availability zone 
+variable "az_name"{
+  default = "us-west-2b"
+  description = "defualt az within us west 2"
+}
 
 resource "aws_redshift_cluster" "default" {
   cluster_identifier = "machi-redshift-cluster"
-  database_name      = "mydb"
-  master_username    = "foo"
-  master_password    = "Mustbe8characters"
+  database_name      = "dev"
+  master_username    = "machi"
+  master_password    = "Master2password"
   node_type          = "dc2.large"
   cluster_type       = "single-node"
   vpc_security_group_ids = ["${aws_security_group.redshift_sg.id}"]
   iam_roles = ["arn:aws:iam::094240567632:role/redshiftonbehalfofme"]
-#  cluster_subnet_group_name = "redshift_subnet"
-#  availability_zone  = "usw2-az4"
-#  automated_snapshot_retention_period  = 1
+  availability_zone  = "${var.az_name}"
+  automated_snapshot_retention_period  = 1
   port = "${var.port_number}"
-  skip_final_snapshot = true
+  skip_final_snapshot = false
+  final_snapshot_identifier = "lastredshiftinstance"
+  tags = {
+    usage = "study"
+  }
 }
 
+#The associated security group
 resource "aws_security_group" "redshift_sg" {
   name        = "redshift_sg"
   description = "Allow Databases traffic"
-  vpc_id = "vpc-c5bd6fbd"
+  vpc_id     = "${var.vpc_id}"
   
   ingress {
     from_port = "${var.port_number}"
@@ -53,9 +66,21 @@ resource "aws_security_group" "redshift_sg" {
     protocol        = "-1"
     cidr_blocks     = ["0.0.0.0/0"]
   }
+  tags = {
+    usage = "study"
+  }
 }
 
 output "sg_id" {
   value = ["${aws_security_group.redshift_sg.id}"]
 }
 
+resource "aws_subnet" "olap" {
+  vpc_id     = "${var.vpc_id}"
+  cidr_block = "172.31.64.0/20"
+  availability_zone  = "${var.az_name}"
+
+  tags = {
+    usage = "study"
+  }
+}
