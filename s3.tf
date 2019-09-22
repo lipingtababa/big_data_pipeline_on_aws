649859f6be4a20998d6e1910efed3ef6b8644487
Ma Chi <hellomarch@gmail.com>
2019-09-22 19:10:57 +0000

    specify s3 bucket should not be deleted if not empty

--- s3.tf
+++ s3.tf
@@ -1,7 +1,8 @@
 resource "aws_s3_bucket" "logger_store" {
 	bucket = "logger-store"
 	acl = "private"
-	tags = { usage = "logging" }
+	tags = { usage = "logging"}
+	force_destroy = false
 }
 
 output "logger_store_arn" {
